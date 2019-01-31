7e95fc4a886b33a3fad3f49b004f172fb156e745
RXstoinkboy <erykludwin@gmail.com>
2019-02-01 00:24:04 +0100

    todo exercise done

--- src/App.js
+++ src/App.js
@@ -4,49 +4,43 @@ import TodoItem from './TodoItem';
 import todosData from './todosData';
 
 const Div = styled.div`
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      background: lightgrey;
-  `
+    display: flex;
+    align-items: center;
+    flex-flow: column;
+`
 
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      list: todosData,
+      todos: todosData
     }
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(id){
-    this.setState(prevState=>{
-      const newList = prevState.list.map(todo => {
+    this.setState(prevState => {
+      const newTodos = prevState.todos.map(todo => {
         if(todo.id === id){
           todo.completed = !todo.completed
         }
         return todo
       })
       return {
-        list: newList
+        todos: newTodos
       }
     })
   }
 
   render(){
-  const todoList = this.state.list.map(item => 
-    <TodoItem 
-      key={item.id} 
-      item={item}
-      handleChange={this.handleChange}
-    />)
-
-  return(
-    <Div>
-      {todoList}
-    </Div>
-  )
-}
+    return(
+      <Div>
+        {this.state.todos.map(item => {
+          return <TodoItem item={item} key={item.id} handleChange={this.handleChange} />
+        })}
+      </Div>
+    )
+  }
 }
 
 export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,30 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-        background: white;
-        border-radius: 5px;
-        padding: 10px 20px;
-        margin: 5px;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: center;
-        max-width: 300px;
-    `
+    border-radius: 10px;
+    padding: 10px;
+    margin: 2px 0;
+    width: 200px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    background: lightblue;
+`
 
-    const Paragraph = styled.p`
-        margin-left: 20px;
-        font-family: tahoma;
-    `
+const Text = styled.p`
+    margin-left: 20px;
+    font-family: 'tahoma';
+`
 
-const TodoItem =(props)=>{
+// END OF STYLES
+
+const TodoItem =(props)=> {
+
+    const newStyle = {
+        color: 'red',
+        textDecoration: 'line-through',
+        color: 'grey',
+        fontStyle: 'italic',
+    }
+
     return(
         <Container>
-            <input 
-                type="checkbox" 
-                checked={props.item.completed} 
-                onChange={()=>{props.handleChange(props.item.id)}} />
-            <Paragraph>{props.item.text}</Paragraph>
+            <input type='checkbox' checked={props.item.completed} onChange={()=>{props.handleChange(props.item.id)}} />
+            <Text style={props.item.completed ? newStyle : null}>{props.item.text}</Text>
         </Container>
     )
 }
